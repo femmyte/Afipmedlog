58df5b062025a03b226960e96678dbdd50b7dee9
femmyte <sanyaoluadefemi@gmail.com>
2023-12-03 13:12:07 +0100

    Adjusted the layout of the main page

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
 		<html lang='en'>
 			<body className={inter.className}>
 				<AppContextProvider>
-					<DashboardLayout>{children}</DashboardLayout>
+					{/* <DashboardLayout> */}
+					{children}
+					{/* </DashboardLayout> */}
 				</AppContextProvider>
 			</body>
 		</html>
