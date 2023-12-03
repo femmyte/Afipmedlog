65b068532e621181c7a40bb95e9656f5a4b903f7
femmyte <sanyaoluadefemi@gmail.com>
2023-12-03 13:19:43 +0100

    Modified the patient overview page

--- src/app/(dashboard)/patient/overview/page.jsx
+++ src/app/(dashboard)/patient/overview/page.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import OverviewHeader from '@/components/pages/overview/OverviewHeader';
+import Cards from '@/components/pages/overview/Cards';
+import Table from '@/components/pages/overview/Table';
+import MedicalRecords from '@/components/pages/overview/MedicalRecords';
+import Doctors from '@/components/pages/overview/Doctors';
+import GeneralDetails from '@/components/pages/overview/GeneralDetails';
+import OverviewAppointments from '@/components/pages/overview/OverviewAppointments';
 
 const page = () => {
-	return <div>Overview page</div>;
+	return (
+		<div>
+			<OverviewHeader />
+			<Cards />
+			<Table />
+			<MedicalRecords />
+			<Doctors />
+			<GeneralDetails />
+			<OverviewAppointments />
+		</div>
+	);
 };
 
 export default page;
