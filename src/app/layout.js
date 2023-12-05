f33012bf305c8206d2b7b6b87b9ecbbe4b25fa98
femmyte <sanyaoluadefemi@gmail.com>
2023-12-05 21:48:02 +0100

    updated the style ans layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
-import { Inter } from 'next/font/google';
 import './globals.css';
 import { AppContextProvider } from '@/state/AppContext';
 import DashboardLayout from '@/components/common/layout/Dashboard';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
 	title: 'Create Next App',
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>
+			<body className=''>
 				<AppContextProvider>
 					{/* <DashboardLayout> */}
 					{children}
