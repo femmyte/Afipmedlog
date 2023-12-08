c61c324aaa6f8870e74210e84a6294a263233d34
femmyte <sanyaoluadefemi@gmail.com>
2023-12-09 00:16:54 +0100

    Created the authorization modal and updated some styles

--- src/components/pages/overview/Doctors.jsx
+++ src/components/pages/overview/Doctors.jsx
@@ -1,37 +1,42 @@
-import React from 'react';
-import OverviewDoctors from './OverviewDoctors';
+import React from "react";
+import OverviewDoctors from "./OverviewDoctors";
 import { GoChevronRight } from "react-icons/go";
 
 const Doctors = () => {
   return (
-    <div className='w-[16.1875rem] h-[9.9375rem] gap-[1.2rem] py-[1.5rem] px-[1rem] shadow-gray-300 shadow-sm p-2'>
-        <div className='flex justify-between'>
-            <h2>Hello Doctors</h2>
-            <div className='flex items-center text-[#145AE2]'>
-              <button className='text-sm'>See All</button>
-              {/* {IconComponent && <IconComponent />} */}
-              <GoChevronRight />
-            </div>
-       </div>
-       <div className='flex'>
-           <OverviewDoctors
-                img = "/images/user.png"
-                name = "Dr Peace"
-                title = "Cardiologist"/>
+    <div
+      className="w-[16.1875rem] h-[9.9375rem] gap-[1.2rem] py-[1.5rem] px-[1rem]  p-2"
+      style={{ boxShadow: "4px 4px 24px 0px rgba(206, 206, 206, 0.24)" }}
+    >
+      <div className="flex justify-between">
+        <h2>Hello Doctors</h2>
+        <div className="flex items-center text-[#145AE2]">
+          <button className="text-sm">See All</button>
+          {/* {IconComponent && <IconComponent />} */}
+          <GoChevronRight />
+        </div>
+      </div>
+      <div className="flex">
+        <OverviewDoctors
+          img="/images/user.png"
+          name="Dr Peace"
+          title="Cardiologist"
+        />
 
-           <OverviewDoctors
-                img = "/images/user.png"
-                name = "Dr Peace"
-                title = "Cardiologist"/>
+        <OverviewDoctors
+          img="/images/user.png"
+          name="Dr Peace"
+          title="Cardiologist"
+        />
 
-           <OverviewDoctors
-                img = "/images/user.png"
-                name = "Dr Peace"
-                title = "Cardiologist"/>
-
-       </div>
+        <OverviewDoctors
+          img="/images/user.png"
+          name="Dr Peace"
+          title="Cardiologist"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Doctors
+export default Doctors;
--- src/components/pages/overview/MedicalRecords.jsx
+++ src/components/pages/overview/MedicalRecords.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
+import React from "react";
 import { GoChevronRight } from "react-icons/go";
-import overviewMedicalRecords from '@/utils/OverviewMedicalRecords';
+import overviewMedicalRecords from "@/utils/OverviewMedicalRecords";
 import { IoIosArrowRoundForward } from "react-icons/io";
-
 
 const MedicalRecords = (props) => {
   return (
-    <div className='w-[18rem] mt-[1rem] shadow-gray-300 shadow-sm p-3'>
-        <div className='flex justify-between'>
-            <h2>History</h2>
-            <div className='flex items-center text-[#145AE2]'>
-              <button className='text-sm'>See All</button>
-              <GoChevronRight />
-            </div>
-       </div>
-       <div className='p-3'>
-            {overviewMedicalRecords.map((item) => (
-                <div key={item.id} className='flex justify-between py-4 px-2'>
-                    <p>{item.records}</p>
-                    <IoIosArrowRoundForward className='text-[#145AE2] cursor-pointer'/>
-                </div>
-            ))}
-       </div>
+    <div
+      className="w-full mt-[1rem] p-3"
+      style={{ boxShadow: "4px 4px 24px 0px rgba(206, 206, 206, 0.24)" }}
+    >
+      <div className="flex justify-between">
+        <h2>Medical Records</h2>
+        <div className="flex items-center text-[#145AE2]">
+          <button className="text-sm">See All</button>
+          <GoChevronRight />
+        </div>
+      </div>
+      <div className="p-3">
+        {overviewMedicalRecords.map((item) => (
+          <div
+            key={item.id}
+            className="flex justify-between py-4 px-2 border-b"
+          >
+            <p>{item.records}</p>
+            <IoIosArrowRoundForward
+              size={25}
+              className="text-[#145AE2] cursor-pointer"
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MedicalRecords
+export default MedicalRecords;
