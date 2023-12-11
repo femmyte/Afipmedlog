5410b91c3e45090716aff9f8ed13ccc2e3f29951
Adeyera <asimolowoelizabeth@gmail.com>
2023-12-11 04:23:55 +0100

    Created the doctors table for the doctors page

---
+++ src/app/(dashboard)/doctor/doctors/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DoctorsFilterPage from '@/components/pages/doctor/DoctorsPage/DoctorsFilterPage'
+
+const page = () => {
+  return (
+    <div>
+        <DoctorsFilterPage />
+    </div>
+  )
+}
+
+export default page
---
+++ src/utils/doctorsData.js
@@ -0,0 +1,114 @@
+const doctorsData = [
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 101
+    },
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 102
+    },
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 103
+    },
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 104
+    },
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 105
+    },
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 106
+    },
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 107
+    },
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 108
+    },
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 109
+    },
+    {
+        doctorsName: 'Dr. Peace Kuda',
+        specialty: 'Cardioloist',
+        department: 'Cardiology',
+        phoneNumber: '+234 814 7100 43',
+        emailAddress: 'phoebe@gmail.com',
+        country: 'Nigeria',
+        dateAdded: 'Dec 04, 23  10:00am',
+        status: 'Active',
+        id: 110
+    },
+]
+
+export default doctorsData
