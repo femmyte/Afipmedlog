635dedfae7868410cf782ef9afce9f3979d8adb1
Adeyera <asimolowoelizabeth@gmail.com>
2023-12-11 14:17:38 +0100

    Added some files

---
+++ src/app/(dashboard)/doctor/history/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HistoryPage from '@/components/pages/doctor/History/HistoryPage'
+import HistoryTable from '@/components/pages/doctor/History/HistoryTable'
+
+const page = () => {
+  return (
+    <div>
+        <HistoryPage />
+        <HistoryTable />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/pages/doctor/History/HistoryTable.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import historyTableData from '@/utils/historytableData';
+import TestTable from './TestTable';
+
+const HistoryTable = () => {
+  return (
+    <div className='mt-[1.5rem]'>
+        <h3 className='text-[1.25rem] font-medium text-[#151515]'>History</h3>
+        <table className='w-full mt-[1rem] text-[#151515]'>
+            <thead>
+                <tr className='text-left text-[0.875rem]'>
+                    <th className='font-normal py-[0.88rem]'>Symptoms</th>
+                    <th className='font-normal py-[0.88rem]'>Treatment</th>
+                    <th className='font-normal py-[0.88rem]'>Prescription</th>
+                    <th className='font-normal py-[0.88rem]'>Patients</th>
+                    <th className='font-normal py-[0.88rem]'>Date</th>
+                    <th className='font-normal py-[0.88rem]'>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {historyTableData.map((item) => (
+                    <tr key={item.id} className='text-[0.75rem]'>
+                        <td className='py-[0.875rem]'>{item.Symptoms}</td>
+                        <td className='py-[0.875rem]'>{item.treatment}</td>
+                        <td className='py-[0.875rem]'>{item.prescription}</td>
+                        <td className='py-[0.875rem]'>{item.patient}</td>
+                        <td className='py-[0.875rem]'>{item.date}</td>
+                        <td className='py-[0.875rem]'>{item.status}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <TestTable />
+    </div>
+  )
+}
+
+export default HistoryTable
